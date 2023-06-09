7c7676d78ec4e34b13f52d829096838ba5e63cea
munadi1406 <munadifathullah123@gmail.com>
2023-06-09 19:47:47 +0800

    test response

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,12 @@
 import { useEffect } from "react";
 import { login } from "../api/login";
 
+
 export default function Login() {
   const auth = async () => {
     try {
       const authCheck = await login("munadifathullah@gmail.com", "Munadi1406!");
-      console.log(authCheck);
-     const rt = document.cookie
-     console.log(rt)
+      console.log(authCheck)
     } catch (error) {
       console.log(error);
     }
